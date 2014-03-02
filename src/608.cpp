faginbagin/ccextractor

commit b2a1f16fcf4780a02736723cdd061ad7d85543ae

Move subset of libpng and zlib to src; eliminate HAVE_LIBPNG ifdefs
Also update linux/build scripts to exclude local libpng and zlib source files
from compilation. The zlib files won't compile with g++.

src/608.cpp
@@ -114,7 +114,7 @@ void clear_eia608_cc_buffer (struct eia608_screen *data)
     {
         memset(data->characters[i],' ',CC608_SCREEN_WIDTH);
         data->characters[i][CC608_SCREEN_WIDTH]=0;		
-        memset (data->colors[i],COL_TRANSPARENT,CC608_SCREEN_WIDTH+1); 
+        memset (data->colors[i],COL_TRANSPARENT,CC608_SCREEN_WIDTH+1);
         memset (data->fonts[i],FONT_REGULAR,CC608_SCREEN_WIDTH+1); 
         data->row_used[i]=0;        
     }
@@ -182,7 +182,7 @@ void delete_to_end_of_row (struct s_write *wb)
 		{
 			// TODO: This can change the 'used' situation of a column, so we'd
 			// need to check and correct.
-	        use_buffer->characters[wb->data608->cursor_row][i]=' ';
+	        use_buffer->characters[wb->data608->cursor_row][i]=' ';			
 			use_buffer->colors[wb->data608->cursor_row][i]=COL_TRANSPARENT;
 			use_buffer->fonts[wb->data608->cursor_row][i]=wb->data608->font;	
 		}
@@ -269,12 +269,10 @@ void write_subtitle_file_footer (struct s_write *wb)
 			enc_buffer_used=encode_line (enc_buffer,(unsigned char *) str);
 			write (wb->fh, enc_buffer,enc_buffer_used);
 			break;
-#ifdef HAVE_LIBPNG
         case OF_SPUPNG:
             write_spumux_footer(wb);
             break;
-#endif
-		default: // Nothing to do. Only SAMI has a footer
+		default: // Nothing to do, no footer on this format
             break;
     }
 }
@@ -301,11 +299,9 @@ void write_subtitle_file_header (struct s_write *wb)
         case OF_RCWT: // Write header
             write (wb->fh, rcwt_header, sizeof(rcwt_header));
             break;
-#ifdef HAVE_LIBPNG
         case OF_SPUPNG:
             write_spumux_header(wb);
             break;
-#endif
         case OF_TRANSCRIPT: // No header. Fall thru
         default:
             break;
@@ -482,11 +478,9 @@ int write_cc_buffer (struct s_write *wb)
             case OF_TRANSCRIPT:
                 wrote_something = write_cc_buffer_as_transcript (data,wb);
                 break;
-#ifdef HAVE_LIBPNG
             case OF_SPUPNG:
                 wrote_something = write_cc_buffer_as_spupng (data,wb);
                 break;
-#endif
             default: 
                 break;
         }
@@ -616,14 +610,14 @@ int roll_up(struct s_write *wb)
     }
     for (int j=0;j<(1+wb->data608->cursor_row-keep_lines);j++)
     {
-        memset(use_buffer->characters[j],' ',CC608_SCREEN_WIDTH);			
-        memset(use_buffer->colors[j],COL_TRANSPARENT,CC608_SCREEN_WIDTH);
+        memset(use_buffer->characters[j],' ',CC608_SCREEN_WIDTH);			        
+		memset(use_buffer->colors[j],COL_TRANSPARENT,CC608_SCREEN_WIDTH);
         memset(use_buffer->fonts[j],FONT_REGULAR,CC608_SCREEN_WIDTH);
         use_buffer->characters[j][CC608_SCREEN_WIDTH]=0;
         use_buffer->row_used[j]=0;
     }
     memset(use_buffer->characters[lastrow],' ',CC608_SCREEN_WIDTH);
-    memset(use_buffer->colors[lastrow],COL_TRANSPARENT,CC608_SCREEN_WIDTH);
+    memset(use_buffer->colors[lastrow],COL_WHITE,CC608_SCREEN_WIDTH);
     memset(use_buffer->fonts[lastrow],FONT_REGULAR,CC608_SCREEN_WIDTH);
 
     use_buffer->characters[lastrow][CC608_SCREEN_WIDTH]=0;
@@ -840,6 +834,8 @@ void handle_command (/*const */ unsigned char c1, const unsigned char c2, struct
 				case COM_ROLLUP4:
 					wb->data608->mode=MODE_ROLLUP_4;
 					break;
+				default: // Impossible, but remove compiler warnings
+					break;
 			}
             break;
         case COM_CARRIAGERETURN:
@@ -1053,7 +1049,7 @@ void handle_pac (unsigned char c1, unsigned char c2, struct s_write *wb)
 		{
 			if (use_buffer->row_used[j])
 			{
-				memset(use_buffer->characters[j],' ',CC608_SCREEN_WIDTH);			
+				memset(use_buffer->characters[j],' ',CC608_SCREEN_WIDTH);							
 				memset(use_buffer->colors[j],COL_TRANSPARENT,CC608_SCREEN_WIDTH);
 				memset(use_buffer->fonts[j],FONT_REGULAR,CC608_SCREEN_WIDTH);
 				use_buffer->characters[j][CC608_SCREEN_WIDTH]=0;
@@ -1237,7 +1233,7 @@ void process608 (const unsigned char *data, int length, struct s_write *wb)
 				{
 					// Duplicate dual code, discard. Correct to do it only in
 					// non-XDS, XDS codes shall not be repeated.
-                    dbg_print(DMT_608, "Skipping command %02X,%02X Duplicate\n", hi, lo);
+					 dbg_print(DMT_608, "Skipping command %02X,%02X Duplicate\n", hi, lo);
                     // Ignore only the first repetition
                     wb->data608->last_c1=-1;
                     wb->data608->last_c2=-1;

linux/Makefile
@@ -1,14 +1,8 @@
 # Comment out the following line if you don't have libpng
-HAVE_LIBPNG = true
 
 CPPFLAGS += -DGPAC_CONFIG_LINUX -D_FILE_OFFSET_BITS=64 -I../src/gpacmp4
-CPPFLAGS += -I../libpng -I../zlib
 CFLAGS += -g -Wno-write-strings
-
-ifdef HAVE_LIBPNG
-	CFLAGS += -DHAVE_LIBPNG
-	LDLIBS += ../libpng/.libs/libpng12.a ../zlib/libz.a
-endif
+LDLIBS += -lpng
 
 CXXFLAGS = $(CFLAGS)
 CC = $(CXX)

linux/build
@@ -1,17 +1,5 @@
 #!/bin/bash
-
-cd ../zlib
-./configure
-make || exit $?
-
-cd ../libpng
-./configure
-make || exit $?
-
-cd ../linux
-g++ -Wno-write-strings \
--DHAVE_LIBPNG -DGPAC_CONFIG_LINUX -D_FILE_OFFSET_BITS=64 \
--I../src/gpacmp4/ -I../libpng -I../zlib \
-$(find ../src/ -name '*.cpp') $(find ../src/ -name '*.c') \
-../libpng/.libs/libpng12.a ../zlib/libz.a \
--o ccextractor
+g++ -Wno-write-strings -DGPAC_CONFIG_LINUX -D_FILE_OFFSET_BITS=64  -I../src/gpacmp4/  -o ccextractor \
+$(find ../src/ -name '*.cpp') \
+$(find ../src/ \( -path ../src/libpng -o -path ../src/zlib \) -prune  -o -name '*.c' -print) \
+-lpng

linux/builddebug
@@ -1,17 +1,5 @@
 #!/bin/bash
-
-cd ../zlib
-./configure
-make || exit $?
-
-cd ../libpng
-./configure
-make || exit $?
-
-cd ../linux
-g++ -Wno-write-strings -g \
--DHAVE_LIBPNG -DGPAC_CONFIG_LINUX -D_FILE_OFFSET_BITS=64 \
--I../src/gpacmp4/ -I../libpng -I../zlib \
-$(find ../src/ -name '*.cpp') $(find ../src/ -name '*.c') \
-../libpng/.libs/libpng12.a ../zlib/libz.a \
--o ccextractor
+g++ -Wno-write-strings -g -DGPAC_CONFIG_LINUX -D_FILE_OFFSET_BITS=64  -I../src/gpacmp4/  -o ccextractor \
+$(find ../src/ -name '*.cpp') \
+$(find ../src/ \( -path ../src/libpng -o -path ../src/zlib \) -prune  -o -name '*.c' -print) \
+-lpng

src/608_helpers.cpp
@@ -180,7 +180,7 @@ unsigned get_decoder_line_encoded (unsigned char *buffer, int line_num, struct e
         int its_col = data->colors[line_num][i];
         if (its_col != col  && !nofontcolor)
         {
-            if (col!=COL_WHITE && col < COL_BLACK) // We need to close the previous font tag
+            if (col!=COL_WHITE && col < COL_BLACK) // We need to close the previous font tag // We need to close the previous font tag
             {
                 buffer+= encode_line (buffer,(unsigned char *) "</font>");
             }

src/608_spupng.cpp
@@ -1,5 +1,3 @@
-#ifdef HAVE_LIBPNG
-
 #include <assert.h>
 #include <sys/stat.h>
 
@@ -38,7 +36,7 @@ write_spumux_footer(struct s_write *wb)
         ((SpuPng*)wb->spupng_data) -> writeFooter();
         delete (SpuPng*)wb->spupng_data;
         wb->spupng_data = 0;
-        wb->fh = -1;
+		wb->fh = -1;
     }
 }
 
@@ -532,7 +530,7 @@ SpuPng::exportPNG(struct eia608_screen* data)
     png_bytep *row_pointer;
     png_bytep image;
     int ww, wh, rowstride, row_adv;
-    int row;
+    int row;    
 
     assert ((sizeof(png_byte) == sizeof(uint8_t))
             && (sizeof(*image) == sizeof(uint8_t)));
@@ -604,4 +602,3 @@ unknown_error:
 // End copy from http://zapping.cvs.sourceforge.net/viewvc/zapping/vbi/src/exp-gfx.c?view=markup&pathrev=zvbi-0-2-33
 //
 
-#endif

src/608_spupng.h
@@ -1,5 +1,4 @@
 #ifndef __608_SPUPNG_H__
-#ifdef HAVE_LIBPNG
 
 #include "png.h"
 #include "ccextractor.h"
@@ -34,5 +33,4 @@ class SpuPng
         int yOffset;
 };
 
-#endif /* HAVE_LIBPNG */
 #endif /* __608_SPUPNG_H__ */

src/608_srt.cpp
@@ -63,7 +63,7 @@ void write_stringz_as_srt (char *string, struct s_write *wb, LLONG ms_start, LLO
     dbg_print(DMT_608, "- - - - - - - - - - - -\r\n");
    
     write (wb->fh, encoded_crlf, encoded_crlf_length);
-    free(el);
+	free(el);
 }
 
 int write_cc_buffer_as_srt (struct eia608_screen *data, struct s_write *wb)

commit eae1ddefcbb8e854ea894462688188e2d41c9a95

Fix regression caused by one line in commit b2a1f16fcf
Somehow a memset to COL_TRANSPARENT was changed to COL_WHITE causing
roll-up captions to be filled with black background to the end of line.

src/608.cpp
@@ -617,7 +617,7 @@ int roll_up(struct s_write *wb)
         use_buffer->row_used[j]=0;
     }
     memset(use_buffer->characters[lastrow],' ',CC608_SCREEN_WIDTH);
-    memset(use_buffer->colors[lastrow],COL_WHITE,CC608_SCREEN_WIDTH);
+    memset(use_buffer->colors[lastrow],COL_TRANSPARENT,CC608_SCREEN_WIDTH);
     memset(use_buffer->fonts[lastrow],FONT_REGULAR,CC608_SCREEN_WIDTH);
 
     use_buffer->characters[lastrow][CC608_SCREEN_WIDTH]=0;